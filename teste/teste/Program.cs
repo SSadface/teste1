d7fb36e04e84badcefbaa044a16e5b60a0efb0fb
DUAN AVELINO FERREIRA <duan.ferreira@alunos.sc.senac.br>
2022-10-14 19:33:51 -0300

    POG POG POG POG

---
+++ teste/teste/Program.cs
@@ -0,0 +1,7 @@
+﻿int num1, num2;
+
+num1 = 30;
+num2 = 39;
+
+Console.WriteLine($"Soma: {num1+num2}\nSubtração: {num1 - num2}\nMultiplicação: {num1 * num2}\nDivisão: {num1 / num2}");
+Console.ReadKey();
